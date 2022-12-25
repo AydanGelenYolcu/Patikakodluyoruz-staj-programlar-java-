f91f7666c46226b54ba609f717d99d006fb88ae4
AydanGelenYolcu <117212805+AydanGelenYolcu@users.noreply.github.com>
2022-12-25 17:55:42 +0300

    Create taximeter.java

---
+++ java101/fundematels and variables/taximeter.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new  Scanner(System.in);
+        int km;
+        double perKm=2.20 , total, StartPrice=10;
+        System.out.print("what km is the distance ");
+        km= input.nextInt();
+        total= (km * per_km);
+        total +=StartPrice;
+        total= (total<20 ? 20 : total);
+        System.out.println("total price: "+ total);
+    }
+}
